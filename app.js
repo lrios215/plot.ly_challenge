84570b30158544b34e34d2e1feae4f933796e6c5
lrios215 <70179261+lrios215@users.noreply.github.com>
2020-12-21 16:46:48 -0500

    update html

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
 //Step 1: Plotly
 
 //1. Use the D3 library to read in `samples.json`.
-
-//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-//* Use `sample_values` as the values for the bar chart.
-
-//* Use `otu_ids` as the labels for the bar chart.
-
-//* Use `otu_labels` as the hovertext for the chart.
-
-
-//3. Create a bubble chart that displays each sample.
-
-// * Use `otu_ids` for the x values.
-
-// * Use `sample_values` for the y values.
-
-// * Use `sample_values` for the marker size.
-
-// * Use `otu_ids` for the marker colors.
-
-// * Use `otu_labels` for the text values.
-
-// 4. Display the sample metadata, i.e., an individual's demographic information.
-
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-//6. Update all of the plots any time that a new sample is selected.
-
-// append ids to the dropdown
 d3.json('samples.json').then((data)=>{
     var id=data.names;
     console.log(data.metadata);
@@ -38,6 +9,10 @@ d3.json('samples.json').then((data)=>{
         select.append('option').text(v);
     })
 })
+//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        //* Use `sample_values` as the values for the bar chart.
+        //* Use `otu_ids` as the labels for the bar chart.
+        //* Use `otu_labels` as the hovertext for the chart.
 function makePlot(testId){
     d3.json('samples.json').then((data)=>{
         // This is the array
@@ -58,7 +33,13 @@ function makePlot(testId){
             orientation:'h'
         }
         Plotly.newPlot('bar',[trace]);
-        // make bubble chart
+
+//3. Create a bubble chart that displays each sample.
+        // * Use `otu_ids` for the x values.
+        // * Use `sample_values` for the y values.
+        // * Use `sample_values` for the marker size.
+        // * Use `otu_ids` for the marker colors.
+        // * Use `otu_labels` for the text values.
         var otuValue=samples.map(row=>row.sample_values);
         var otuValue=otuValue[testNum];
         var otuId=samples.map(row=>row.otu_ids);
@@ -91,8 +72,13 @@ function makePlot(testId){
                 }
             },
         };
-        Plotly.newPlot('bubble',data1,bubbleLayout);   
-        // make gauge chart
+        Plotly.newPlot('bubble',data1,bubbleLayout); 
+
+// 4. Display the sample metadata, i.e., an individual's demographic information.
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+//6. Update all of the plots any time that a new sample is selected.
+// append ids to the dropdown
+  //BONUS: make gauge chart
         var meta=data.metadata;
         var data2 = [
             {
